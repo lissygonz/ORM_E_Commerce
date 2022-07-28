73dae836b9052b6dfd277035c80b8f39aadb2e50
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-07-28 17:57:46 -0400

    added missing tag id

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -18,6 +18,13 @@ ProductTag.init(
             key: 'id'
         },
     },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'tag',
+            key: 'id'
+        },
+    },
   },
   {
     sequelize,
